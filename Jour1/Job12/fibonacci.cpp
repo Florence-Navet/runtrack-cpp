ebb58b68701c46f3f1b7060faee9d7e7af406d36
Florence <enolaworld@gmail.com>
2025-11-04 19:54:26 +0100

    feat: job12 done

--- Jour1/Job12/fibonacci.cpp
+++ Jour1/Job12/fibonacci.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
+#include <cstdint>
 using namespace std;
 
-int fibonacci(int n) {
+
+// Créer une fonction fibonacci qui calcule le
+// N-ième terme de la suite de Fibonacci. Le résultat
+// doit être calculé par le compilateur lors du
+// traitement du code, et non lors de son exécution.
+constexpr int fibonacci(int n) {
     if (n<= 1)
     return n;
     return fibonacci(n-1) + fibonacci(n-2);
+}
+
+// Écrire ensuite une fonction qui prend un entier non-signé sur 32 bits en entrée
+// et retourne vrai si le nombre de bits à 1 est pair et faux sinon. Testons ta
+// capacité à démêler les mystères de cette suite et d’anticiper ses séquences
+// numériques.
+bool numberDeBit(uint32_t n) {
+    int count = 0;
+    while (n > 0) {
+        if (n & 1)
+        count++;
+        n >>=1;
+    }
+    return (count % 2 ==0);  
 }
 
 
@@ -17,12 +37,24 @@ int main() {
     }
     cout << endl;
 
+    cout << "Verification du nombre de bit à 1 " << endl;
+
+    uint32_t nombre;
+    cout << "Entrez un nombre non signé de (0 à 294 967 295) : ";
+    cin >> nombre;
+
+    if (numberDeBit(nombre)) 
+        cout << nombre << " a un nombre PAIR de bits à 1" << endl;
+
+    else 
+        cout << nombre << " a un nombre IMPAIR de bits à 1" << endl;
+    
+
+
 
    return 0;
 }
 
 
-// Créer une fonction fibonacci qui calcule le
-// N-ième terme de la suite de Fibonacci. Le résultat
-// doit être calculé par le compilateur lors du
-// traitement du code, et non lors de son exécution.
+
+
